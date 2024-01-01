b4a9567dc783564be9743dd38954aee4b98977a3
Juma Antony <jumaantony840@gmail.com>
2024-01-01 17:23:30 +0300

    add endpoints to fetch user and update user details

--- apps/api/backend/src/user/user.controller.ts
+++ apps/api/backend/src/user/user.controller.ts
@@ -1,4 +1,71 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserService } from '@user/user.service';
+import { UpdateUserDetailsDto } from '@user/dto/update-user-details.dto';
+import { Profile } from '@user/entities/profile.entity';
+import { IsUserGuard } from '@common/guards/is-user.guard';
+import { userResponseExample } from '@user/responses/user-response-examples';
+import { IsAuthenticatedUserGuard } from '@common/guards/is-authenticated-user.guard';
 
-@Controller('user')
-export class UserController {}
+@ApiTags('User')
+@Controller({ path: 'user', version: '1' })
+export class UserController {
+  constructor(private readonly _userService: UserService) {}
+
+  @Get(':userId')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get user details' })
+  @ApiResponse({
+	status: HttpStatus.OK,
+	description: 'User details have been successfully retrieved.',
+	content: {
+		'application/json': {
+			example: userResponseExample
+		},
+	},
+  })
+  @UseGuards(IsAuthenticatedUserGuard)
+  public async getUserDetails(
+	@Param('userId') userId: string,
+  ): Promise<Profile> {
+	return await this._userService.findUserById(userId);
+  }
+
+  @Patch(':userId')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update user details' })
+  @ApiBody({ type: UpdateUserDetailsDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'User details have been successfully updated.',
+    content: {
+    	'application/json': {
+    		example: userResponseExample
+    	},
+    },
+  })
+  @UseGuards(IsUserGuard)
+  public async updateUserDetails(
+    @Param('userId') userId: string,
+    @Body() updateUserDetailsDto: UpdateUserDetailsDto,
+  ): Promise<Profile> {
+    return await this._userService.updateUserDetails(
+      userId,
+      updateUserDetailsDto,
+    );
+  }
+}
