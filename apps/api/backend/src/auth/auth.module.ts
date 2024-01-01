db2e26b31a007d61b713ba0cf8764154762c595b
Juma Antony <jumaantony840@gmail.com>
2024-01-01 11:06:55 +0300

    perform dependacy injection

--- apps/api/backend/src/auth/auth.module.ts
+++ apps/api/backend/src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from '@auth/auth.service';
 import { AuthController } from '@auth/auth.controller';
 import { AuthRepository } from '@auth/auth.repository';
 import { SupabaseModule } from '@common/supabase/supabase.module';
+import { UserModule } from '@user/user.module';
+import { UserService } from '@user/user.service';
 
 @Module({
-  imports: [SupabaseModule],
-  providers: [AuthService, AuthRepository],
+  imports: [SupabaseModule, UserModule],
+  providers: [AuthService, AuthRepository, UserService],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- apps/api/backend/src/user/user.module.ts
+++ apps/api/backend/src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from '@user/user.service';
 import { UserController } from '@user/user.controller';
+import { UserRepository } from '@user/user.respository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile } from '@user/entities/profile.entity';
 
 @Module({
-  providers: [UserService],
+  imports: [TypeOrmModule.forFeature([Profile])],
+  providers: [UserService, UserRepository],
   controllers: [UserController],
+  exports: [UserService, UserRepository],
 })
 export class UserModule {}
