c388239e4bb42cbd2c553f82511982602625b312
Juma Antony <jumaantony840@gmail.com>
2024-01-02 10:18:13 +0300

    handle profile upload business logic

--- apps/api/backend/src/user/user.service.ts
+++ apps/api/backend/src/user/user.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '@user/user.respository';
 import { Profile } from '@user/entities/profile.entity';
-import { UpdateUserDetailsDto } from '@user/dto/update-user-details.dto';
+import { MediaService } from '@common/media/media.service';
+import { IUserUpdate } from '@user/interface/user.interface';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly _userRepository: UserRepository) {}
+  constructor(
+    private readonly _userRepository: UserRepository,
+    private readonly _mediaService: MediaService,
+    ) {}
 
   public async findUserByUsername(username: string): Promise<Profile> {
     return await this._userRepository.findUserByUsername(username);
@@ -21,12 +25,29 @@ export class UserService {
 
   public async updateUserDetails(
     userId: string,
-    updateUserDetailsDto: UpdateUserDetailsDto,
+    updateUserDetailsDto: IUserUpdate,
   ): Promise<Profile> {
     await this._userRepository.findUserById(userId);
     return await this._userRepository.updateUserDetails(
       userId,
       updateUserDetailsDto,
     );
+  }
+
+  public async uploadProfileImage(
+    userId: string,
+    storageBucketName: string,
+    file: Express.Multer.File,
+  ): Promise<any> {
+    await this._userRepository.findUserById(userId);
+    const response = await this._mediaService.uploadProfileImage(
+      userId,
+      storageBucketName,
+      file,
+    );
+    await this._userRepository.updateUserDetails(userId, {
+      profile_photo_url: response.publicUrl,
+    });
+    return response;
   }
 }
