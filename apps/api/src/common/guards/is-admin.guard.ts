d5d87433fcc46ceaf8b78f056b692c486616aa2e
Juma Antony <jumaantony840@gmail.com>
2024-01-06 21:56:15 +0300

    add is admin gueard

---
+++ apps/api/src/common/guards/is-admin.guard.ts
@@ -0,0 +1,40 @@
+import {
+	CanActivate,
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
+  } from '@nestjs/common';
+  import { extractTokenFromHeader } from '@common/token/extract-token';
+  import { decodeToken } from '@common/token/decode-token';
+  import { UserRepository } from '@user/user.respository';
+  import { BaseException } from '@common/exceptions/base.exception';
+  
+  @Injectable()
+  export class IsAdminGuard implements CanActivate {
+	constructor(private readonly _userRepository: UserRepository) {}
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+	  const request = context.switchToHttp().getRequest();
+	  const token = extractTokenFromHeader(request.headers);
+	  if (!token) {
+		throw new UnauthorizedException(
+		  'A valid Bearer token is required to access this resource',
+		);
+	  }
+	  try {
+		const decodedToken = decodeToken(token);
+		const userId = decodedToken.sub;
+		const userData = await this._userRepository.findUserById(userId);
+		const userEmail = userData.email;
+		const userRole = decodedToken.role;
+		if (userId && userRole === 'service_role') {
+		  request['userId'] = decodedToken.sub;
+		  request['userEmail'] = userEmail;
+		  return true;
+		}
+	  } catch (error) {
+		throw new BaseException(error.message);
+	  }
+	  return false;
+	}
+  }
+  
