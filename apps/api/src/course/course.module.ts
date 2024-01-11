fd47c5a153144ab2f9e55e0e5492c916d62fb347
Juma Antony <jumaantony840@gmail.com>
2024-01-11 21:38:36 +0300

    add module dependancies

--- apps/api/src/course/course.module.ts
+++ apps/api/src/course/course.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CourseService } from '@course/course.service';
 import { CourseController } from '@course/course.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from '@course/entities/course.entity';
+import { Subject } from '@subject/entities/subject.entity';
+import { CourseRepository } from '@course/course.repository';
+import { UserRepository } from '@user/user.respository';
+import { Profile } from '@user/entities/profile.entity';
+import { MediaModule } from '@common/media/media.module';
 
 @Module({
-  providers: [CourseService],
-  controllers: [CourseController]
+  imports: [TypeOrmModule.forFeature([Course, Subject, Profile]), MediaModule],
+  providers: [CourseService, CourseRepository, UserRepository],
+  controllers: [CourseController],
 })
 export class CourseModule {}
