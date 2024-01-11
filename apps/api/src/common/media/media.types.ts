ad8371e2cd6de0f2b8ecd9bdf7310181a943aa80
Juma Antony <jumaantony840@gmail.com>
2024-01-11 16:34:28 +0300

    add a response interface

--- apps/api/src/common/media/media.types.ts
+++ apps/api/src/common/media/media.types.ts
@@ -6,3 +6,10 @@ export interface IUploadMediaResponse {
   path: string;
   publicUrl: string;
 }
+
+export interface ImageUploadResponse {
+  path: string;
+  id: string;
+  fullPath: string;
+  publicUrl: string;
+}
